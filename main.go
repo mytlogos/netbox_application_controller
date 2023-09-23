57049c94c0bcce6ebd2da4788f542918e420a755
mytlogos <mytlogos@hotmail.de>
2023-09-23 12:16:09 +0200

    remove old files

--- main.go
+++ main.go
@@ -5,12 +5,10 @@ import (
 	"log"
 
 	v1 "github.com/mytlogos/netbox_application_controller/api/v1"
-	"github.com/mytlogos/netbox_application_controller/controllers"
 	"github.com/mytlogos/netbox_application_controller/services"
 	"github.com/mytlogos/netbox_application_controller/services/backend"
 
 	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/mvc"
 	"github.com/kataras/iris/v12/versioning"
 
 	"github.com/kataras/iris/v12/middleware/accesslog"
@@ -78,30 +76,6 @@ func run(config Config, storage *services.Storage) {
 	app.UseRouter(recover.New())
 	// Group routes and mvc apps based on /api path prefix.
 	api := app.Party("/api")
-	{
-		// Group based on /api/counter path prefix.
-		counterAPI := api.Party("/counter")
-		// Optionally, a <trick> to keep the `m` local variable
-		// unaccessible outside of this block's scope. That
-		// way you can register many mvc apps for different Parties
-		// with a "m" variable.
-		// Alternatively you can use the mvc.Configure function :)
-
-		// Register a new MVC Application to the counterAPI Party.
-		m := mvc.New(counterAPI)
-		m.Register(
-			// Register a static dependency (static because it doesn't accept an iris.Context,
-			// only one instance of that it's used). Helps us to keep a global counter across
-			// clients requests.
-			services.NewGlobalCounter(),
-			// Register a dynamic dependency (GetFields accepts an iris.Context,
-			// it binds a new instance on every request). Helps us to
-			// set custom fields based on the request handler.
-			accesslog.GetFields,
-		)
-		// Register our controller.
-		m.Handle(new(controllers.Counter))
-	}
 
 	api.UseRouter(versioning.Aliases(versioning.AliasMap{
 		versioning.Empty: "1.0.0",
